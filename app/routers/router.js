d3470c369ba15da636ab9f3823ed4fe3ce1c9ecf
Silversable16 <Jponcet@miumg.edu.gt>
2024-08-15 23:46:17 -0600

    6

--- app/routers/router.js
+++ app/routers/router.js
@@ -8,11 +8,15 @@ router.get('/api/libros/onebycodigo/:codigo', libros.getLibroByCodigo);
 router.put('/api/libros/update/:codigo', libros.updateByCodigo);
 router.delete('/api/libros/delete/:codigo', libros.deleteByCodigo);
 
-const prestamo = require('../controllers/prestamo.controller.js');
-router.post('/api/prestamos/create', prestamo.create);
-router.get('/api/prestamos/all', prestamo.retrieveAllPrestamos);
-router.get('/api/prestamos/onebyid/:id', prestamo.getPrestamoById);
-router.put('/api/prestamos/update/:id', prestamo.updateById);
-router.delete('/api/prestamos/delete/:id', prestamo.deleteById);
+const prestamos = require('../controllers/prestamo.controller.js');
+
+router.post('/api/prestamos/create', prestamos.create);
+router.get('/api/prestamos/all', prestamos.retrieveAllPrestamos);
+router.get('/api/prestamos/onebyid/:id', prestamos.getPrestamoById);
+router.put('/api/prestamos/update/:id', prestamos.updateById);
+router.delete('/api/prestamos/delete/:id', prestamos.deleteById);
+
+module.exports = router;
+
 
 module.exports = router;
